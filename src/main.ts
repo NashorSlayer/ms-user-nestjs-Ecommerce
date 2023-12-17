577ac90288e666c96ad9c9cd28f91a61d6ce1be3
NashorSlayer <arif.bordons@alumnos.ucn.cl>
2023-12-17 19:53:15 -0300

    Remove logger option and unnecessary imports in main.ts and auth.controller.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
-    logger: [],
     transport: Transport.RMQ,
     options: {
       urls: [process.env.RMQ_URL],
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AuthMsg } from 'src/utils/constants';

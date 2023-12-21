a50ee7dfe626280333121d62909b221b71ac0434
NashorSlayer <arif.bordons@alumnos.ucn.cl>
2023-12-20 23:31:11 -0300

    Add RabbitMQ constant for queue name

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
+import { RabbitMQ } from './utils/constants';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.RMQ,
     options: {
       urls: [process.env.RMQ_URL],
-      queue: process.env.RMQ_QUEUE,
+      queue: RabbitMQ.UserQueue,
     }
   })
   await app.listen();
